7df09ac21a1343474a73e5e78d294ed6d738ab69
Vasya Semenov <monstrodev@gmail.com>
2014-11-06 19:38:12 +0200

    Added minus function

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ var plus = function(a, b) {
 	return a + b
 };
 
-var func = function(a, action, b) {
+var minus = function(a, b) {
+	return a - b;
+};
 
+var func = function(a, action, b) {
 	if(action === '+') {
 		return plus(a, b);
 	}
 
+	if(action === '-') {
+		return minus(a, b);
+	}
 };
+
 console.log(func(2, '+', 5));
+console.log(func(3, '-', 5));
